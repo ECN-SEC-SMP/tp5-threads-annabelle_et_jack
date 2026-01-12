86e9f3f615a22605e0cc7526cb7d529112024298
Jaaackoo <jack.hascoet@gmail.com>
2026-01-12 17:40:24 +0100

    question 2.1

--- src/threadTrains/Controleur.hpp
+++ src/threadTrains/Controleur.hpp
@@ -1,38 +1,68 @@
 #ifndef CONTROLEUR_HPP
 #define CONTROLEUR_HPP
 
-#include <thread>             
+#include <thread>
 #include <iostream>
 #include <mutex>
 #include <condition_variable>
 
-using namespace std; 
+using namespace std;
 
-class Controleur {
-  public:
-    Controleur (int valeur_initiale) : 
-    val(valeur_initiale)
+class Controleur
+{
+public:
+  Controleur(int valeur_initiale) : val(valeur_initiale)
+  {
+    this->sens = true;
+  }
+
+  bool controlinEnB(int numero)
+  {
+    if (this->val == 0)
     {
-    }
-    
-    bool controlinEnB(int numero) { 
-        return true;
-    }  
-
-    bool controlinEnA(int numero) { 
-        return true;
-    }  
-
-    bool controloutEnB(int numero){ 
-      return true ;
-    }
-
-    bool controloutEnA(int numero){ 
+      this->val++;
+      this->sens = true;
       return true;
     }
+    else if (this->val > 0 && this->sens == true && )
+    {
+      this->val++;
+      return true;
+    }
+    return false;
+  }
 
-  private:
-    int val;
+  bool controlinEnA(int numero)
+  {
+    if (this->val == 0)
+    {
+      this->val++;
+      this->sens = false;
+      return true;
+    }
+    else if (this->val > 0 && this->sens == false)
+    {
+      this->val++;
+      return true;
+    }
+    return false;
+  }
+
+  bool controloutEnB(int numero)
+  {
+    this->val--;
+    return true;
+  }
+
+  bool controloutEnA(int numero)
+  {
+    this->val--;
+    return true;
+  }
+
+private:
+  bool sens;
+  int val;
 };
 
-#endif //CONTROLEUR_HPP
+#endif // CONTROLEUR_HPP
