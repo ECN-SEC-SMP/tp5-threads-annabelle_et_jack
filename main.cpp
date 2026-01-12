e6666bfa60c47fc315fdc5af10f261c2edc3d156
Jaaackoo <jack.hascoet@gmail.com>
2026-01-12 16:25:49 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file main.cpp
+ * @brief
+ * @version 0.1
+ * @date 2026-01-012
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "==============================" << "\n";
+    std::cout << "CPP TP5 - Anna Jack " << "\n";
+    std::cout << "==============================" << "\n";
+    std::cout << std::endl;
+
+    /* TODO */
+
+    return 0;
+}
